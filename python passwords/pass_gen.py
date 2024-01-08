de24a810bc1c5cb323d40cb890bce32a6117a426
TheBigOcean <jlo0015@mix.wvu.edu>
2024-01-08 13:49:45 -0500

    Added hashing and shadow files writing

--- python passwords/pass_gen.py
+++ python passwords/pass_gen.py
@@ -1,14 +1,25 @@
 import random
-import string 
+import string
+import bcrypt 
 
 print("Welcome to the Small Wave password generator!")
 
 username = input("Please enter the email/username of the password you are using.")
+
 word_length = int(input("Please enter the length of your password."))
 
 components = string.ascii_letters + string.digits + string.punctuation
 
-password = "".join(random.choices(components, k=word_length))
+password = ''.join(random.choices(components, k=word_length)).encode('utf-8')
+
+hashed = bcrypt.hashpw(password, bcrypt.gensalt( 12 ))
+
+print("Your password is" + password.decode())
 
 f = open("passwords.txt", "a")
-f.write("Username: " + username + " " + "Password: " + password + "\n")
+f.write("User: " + username + " | " + "Hashed Password: " + hashed.decode() + "\n")
+f.write(" -------------------------------------------------------------------------------------------- ")
+
+s = open("shadow.txt", "a")
+s.write("User: " + username + " | " + "Plaintext Password: " + password.decode() + "\n")
+s.write(" -------------------------------------------------------------------------------------------- ")
